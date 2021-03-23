b19cb7e51ec68aefdeebb085349bfa9372a54bfb
Saleem Hadad <saleem.hadad@talabat.com>
2021-03-23 23:56:15 +0400

    wip

--- src/js/admin.js
+++ src/js/admin.js
@@ -2,6 +2,7 @@ let recipes = require('./recipes.json');
 
 const renderRecipesToTable = (recipes) => {
     // do the logic to add the recipes to the table
+    // 
 }
 
 renderRecipesToTable(recipes);
